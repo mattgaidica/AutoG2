31f4ee043a56663368d5f29b936ce768ea6dc48a
Matt Gaidica <matt@gaidi.ca>
2022-10-24 19:05:39 -0400

    Tic works up/down, flipped screen orientation, need to close the ADC-motor loop

--- AutoG2.ino
+++ AutoG2.ino
@@ -24,8 +24,10 @@ const int MENU_SET = 1;
 const int MENU_MANUAL = 2;
 const int MENU_DEBUG = 3;
 const int MENU_HOME = 4;
-const String MENU_NONE = ".";
-const String WELCOME_MSG = "Hello, Matt.";
+const String MENU_NONE = "-";
+const String WELCOME_MSG = "Hello, Matt";
+const int LED_BRIGHTNESS = 50;
+const int SCREENSAVER_TIMEOUT = 1000 * 10;
 // Text settings
 const int TEXT_SIZE = 2;
 const int MID = 120;
@@ -40,38 +42,44 @@ long int touchMsElapsed[5] = { 0 };
 long int refreshTime = 0;
 int iLED = 0;
 bool LEDdir = 1;
+bool lightOn = false;
+int16_t adcVal = 0;
 // Calibration
 int calibrationWeights[3] = { 0, 100, 200 };
 int calibrationADC[3] = { 0 };
 const int CAL_NVS_ADDR = 0;
 double linReg[2];
+// motor
+bool motorActive = false;
+const int MOTOR_STEPS_PER_S = 5000000;
+const uint16_t currentLimitWhileMoving = 500;
+const uint16_t currentLimitWhileStopped = 0;
 
 void setup() {
   Serial.begin(9600);
-  carrier.noCase();
   carrier.begin();
+  carrier.display.setRotation(0);
 
   clearScreen();  // default is white text
   carrier.display.setTextSize(TEXT_SIZE);
-  centerString("Init...", MID, MID);  // use +/-ROW
-  // carrier.display.setTextSize(SMALL_TEXT);
+  carrier.display.setCursor(120, 120);
+  carrier.display.print("+");               // mark center
+  centerString("Init...", MID, MID - ROW);  // use +/-ROW
 
-  carrier.leds.setBrightness(50);
+  carrier.leds.setBrightness(LED_BRIGHTNESS);
 
   ADS.begin();
-  while (!ADS.isConnected()) {
-    Serial.println("No conn");
-  }
-  ADS.setGain(16);
+  if (ADS.isConnected()) ADS.setGain(16);
 
   loadCalibrationValues();
+  tic.setProduct(TicProduct::T825);
   homeMenu();
 }
 
 void loop() {
-  buttonsUpdate();
+  buttonsUpdate();  // !!has to run everywhere
 
-  if (doRefresh(10000)) {
+  if (doRefresh(SCREENSAVER_TIMEOUT)) {
     screenSaver();
   }
 
@@ -82,17 +90,66 @@ void loop() {
   if (touch[MENU_HOME]) refreshTime = millis();
 }
 
+// has to run everywhere: updates buttons, motor keep-alive
+void buttonsUpdate() {
+  // clear all
+  for (int i = 0; i < 5; i++) {
+    if (millis() - touchMsElapsed[i] > TOUCH_TIMEOUT_MS) {
+      touch[i] = false;
+      touchMsElapsed[i] = 0;
+    }
+  }
+  for (int i = 0; i < 5; i++) {
+    if (analogRead(i) > TOUCH_THRESH) {
+      touch[i] = true;
+      touchMsElapsed[i] = millis();
+    }
+  }
+  // LED display
+  if (lightOn) {
+    carrier.leds.clear();
+    for (int i = 0; i < 5; i++) {
+      carrier.leds.setPixelColor(i, 255, 255, 255);
+    }
+    carrier.leds.setBrightness(255);
+    carrier.leds.show();
+  } else {
+    carrier.leds.clear();
+    for (int i = 0; i < 5; i++) {
+      if (menuMask[i]) {
+        if (touch[i]) {
+          carrier.leds.setPixelColor(i, 0, 255, 0);
+        } else {
+          carrier.leds.setPixelColor(i, iLED, 0, 0);
+        }
+      }
+      carrier.leds.setBrightness(LED_BRIGHTNESS);
+      carrier.leds.show();
+    }
+  }
+
+  if (motorActive) {
+    tic.resetCommandTimeout();
+  }
+
+  if (iLED >= 200) LEDdir = 0;
+  if (iLED == 20) LEDdir = 1;
+  if (LEDdir) iLED++;
+  if (!LEDdir) iLED--;
+}
+
 void screenSaver() {
   debounceMenu();
   makeButtonMenu("", "", "", "", "");
   carrier.display.setTextSize(1);
   carrier.display.setTextColor(ST77XX_GREEN);
-  int x, y, i = 0;
+  int x, i = 0;
+  int y = 100;
 
   while (1) {
     buttonsUpdate();
 
-    if (y > random(14,24)) {
+    if (y > random(14, 36)) {
       x = random(0, 24);
       y = 0;
       i++;
@@ -120,50 +177,17 @@ void screenSaver() {
   }
 }
 
-void buttonsUpdate() {
-  // clear all
-  for (int i = 0; i < 5; i++) {
-    if (millis() - touchMsElapsed[i] > TOUCH_TIMEOUT_MS) {
-      touch[i] = false;
-      touchMsElapsed[i] = 0;
-    }
-  }
-  for (int i = 0; i < 5; i++) {
-    if (analogRead(i) > TOUCH_THRESH) {
-      touch[i] = true;
-      touchMsElapsed[i] = millis();
-    }
-  }
-  // LED display
-  carrier.leds.clear();
-  for (int i = 0; i < 5; i++) {
-    if (menuMask[i]) {
-      if (touch[i]) {
-        carrier.leds.setPixelColor(i, 0, 255, 0);
-      } else {
-        carrier.leds.setPixelColor(i, iLED, 0, 0);
-      }
-    }
-    carrier.leds.show();
-  }
-
-  if (iLED >= 200) LEDdir = 0;
-  if (iLED == 20) LEDdir = 1;
-  if (LEDdir) iLED++;
-  if (!LEDdir) iLED--;
-}
-
 // MENUS
 void homeMenu() {
   debounceMenu();
   centerString(WELCOME_MSG, MID, MID);  // use +/-ROW
-  makeButtonMenu("Cal", "Set", "Ctrl", "Debug", "Home");
+  makeButtonMenu("Cal", "Set", "Motor", "Debug", "Home");
 }
 
 void calibrateLoad() {
   bool doOnce = true;
   debounceMenu();
-  makeButtonMenu("Unloaded", "100gr", "200gr", "Save", "Home");
+  makeButtonMenu("0gr", "100gr", "200gr", "Save", "Home");
 
   while (1) {
     buttonsUpdate();
@@ -174,15 +198,15 @@ void calibrateLoad() {
     }
     if (touch[0] | touch[1] | touch[2]) {
       if (doRefresh(250)) {
-        int16_t loadVal = ADS.readADC_Differential_0_1();
+        if (ADS.isConnected()) adcVal = ADS.readADC_Differential_0_1();
         for (int i = 0; i < 3; i++) {
-          if (touch[i]) calibrationADC[i] = loadVal;
+          if (touch[i]) calibrationADC[i] = adcVal;
         }
         showCalibrationValues();  // can change but do not save automatically
       }
     }
     if (touch[3]) {  // save to flash
-      centerString("SAVED", MID, MID + ROW * 2);
+      centerString("SAVED", MID, MID + ROW);
       saveCalibrationValues();
       delay(500);
       showCalibrationValues();  // flash save, clear data area
@@ -223,8 +247,8 @@ void showCalibrationValues() {
   char buffer[30];
   sprintf(buffer, "y = %1.1fx + %1.1f", linReg[0], linReg[1]);
   clearDataArea();
-  centerString(calVals, MID, MID);
-  centerString(String(buffer), MID, MID + ROW);
+  centerString(calVals, MID, MID - ROW);
+  centerString(String(buffer), MID, MID);
 }
 
 void setUnload() {
@@ -244,12 +268,44 @@ void setUnload() {
 
 void manualControl() {
   debounceMenu();
-  makeButtonMenu(MENU_NONE, "UP", MENU_NONE, "DOWN", "Home");
 
+  bool doOnce = true;
   while (1) {
     buttonsUpdate();
-    // do stuff
+    if (doOnce) {
+      doOnce = false;
+      if (motorActive) {
+        makeButtonMenu("Turn Off", "DOWN", MENU_NONE, "UP", "Home");
+      } else {
+        makeButtonMenu("Turn On", MENU_NONE, MENU_NONE, MENU_NONE, "Home");
+      }
+    }
 
+    if (touch[0]) {
+      if (motorActive) {
+        tic.setCurrentLimit(currentLimitWhileStopped);
+        tic.enterSafeStart();  // !!check if working
+        motorActive = false;
+      } else {
+        motorActive = true;
+        tic.setCurrentLimit(currentLimitWhileMoving);
+        tic.exitSafeStart();
+      }
+      debounceMenu();
+      doOnce = true;
+    }
+    if (motorActive) {
+      if (touch[1]) {
+        tic.setTargetVelocity(MOTOR_STEPS_PER_S);
+        // carrier.Buzzer.sound(3000);
+      } else if (touch[3]) {
+        tic.setTargetVelocity(-MOTOR_STEPS_PER_S);
+        // carrier.Buzzer.sound(2000);
+      } else {
+        tic.setTargetVelocity(0);  // always send 0 if not up/down
+        // carrier.Buzzer.noSound();
+      }
+    }
     if (touch[MENU_HOME]) {
       homeMenu();
       return;
@@ -259,20 +315,33 @@ void manualControl() {
 
 void debugMode() {
   debounceMenu();
-  makeButtonMenu(MENU_NONE, MENU_NONE, MENU_NONE, MENU_NONE, "Home");
+  makeButtonMenu("Light", MENU_NONE, MENU_NONE, MENU_NONE, "Home");
+
+  bool doOnce = true;
   while (1) {
     buttonsUpdate();
 
-    if (doRefresh(200)) {
+    if (doRefresh(2000) | doOnce) {
+      doOnce = false;
       clearDataArea();
       float temperature = carrier.Env.readTemperature(FAHRENHEIT);
       float humidity = carrier.Env.readHumidity();
       char buffer[30];
-      sprintf(buffer, "%1.0fF, %1.0f%%", temperature, humidity);
-      int16_t loadVal = ADS.readADC_Differential_0_1();
-
+      if (ADS.isConnected()) ADS.readADC_Differential_0_1();
+      sprintf(buffer, "t: 0x%X, %is", millis() / 1000, millis() / 1000);
+      centerString(buffer, MID, MID - ROW);
+      sprintf(buffer, "Wx: %1.0fF, %1.0f%%", temperature, humidity);
       centerString(buffer, MID, MID);  // use +/-ROW
-      centerString(String(loadVal), MID, MID + ROW);
+      centerString("load: " + String(adcVal), MID, MID + ROW);
+    }
+
+    if (touch[0]) {
+      if (lightOn) {
+        lightOn = false;
+      } else {
+        lightOn = true;
+      }
+      delay(200);  // debounce inline
     }
 
     if (touch[MENU_HOME]) {
@@ -328,11 +397,11 @@ void rightString(const String &buf, int x, int y) {
 }
 void makeButtonMenu(const String &buf0, const String &buf1, const String &buf2, const String &buf3, const String &buf4) {
   carrier.display.setTextColor(ST77XX_RED);
-  rightString(buf0, 220, 70);
-  rightString(buf1, 210, 190);
-  centerString(buf2, 120, 225);
-  leftString(buf3, 30, 190);
-  leftString(buf4, 20, 70);
+  leftString(buf0, 30, 190);
+  leftString(buf1, 30, 50);
+  centerString(buf2, 120, 15);
+  rightString(buf3, 210, 50);
+  rightString(buf4, 210, 190);
   carrier.display.setTextColor(ST77XX_WHITE);
 
   // handle mask
